3a9187defde6d6e16eaba1925f560e40e336db91
Ellen Chang <imnunu@gmail.com>
2017-03-13 19:16:11 +0000

    with buffered strings

---
+++ printingwithBufferedStrings.js
@@ -0,0 +1,18 @@
+var https = require('https');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+https.get(requestOptions, function(response) {
+  var bodyChunks = [];
+  response.on('data', function(chunk){
+    bodyChunks.push(chunk);
+  }).on('end', function() {
+    var body = Buffer.concat(bodyChunks);
+    console.log('BODY: ' + body);
+  })
+});
+
+
