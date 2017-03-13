e59a207ba12cdfbfd4716fb3c1a6e9664d6ee35f
Ellen Chang <imnunu@gmail.com>
2017-03-13 19:05:06 +0000

    added \n

--- getAndPrintHTMLChunks.js
+++ getAndPrintHTMLChunks.js
@@ -1,3 +1,21 @@
+// var https = require('https');
+
+// var requestOptions = {
+//     host: 'sytantris.github.io',
+//     path: '/http-examples/step1.html'
+//   };
+
+// https.get(requestOptions, function(response) {
+//   var bodyChunks = [];
+//   response.on('data', function(chunk){
+//     bodyChunks.push(chunk);
+//   }).on('end', function() {
+//     var body = Buffer.concat(bodyChunks);
+//     console.log('BODY: ' + body);
+//   })
+// });
+
+
 var https = require('https');
 
 var requestOptions = {
@@ -5,15 +23,29 @@ var requestOptions = {
     path: '/http-examples/step1.html'
   };
 
-https.get(requestOptions, function(response) {
-  var bodyChunks = [];
+var getAndPrintHTMLChunks = function(response) {
+// https.get(requestOptions, function(response) {
+
   response.on('data', function(chunk){
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-    var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
+    // console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString() + '\n');
   })
-});
+};
+
+https.request(requestOptions, getAndPrintHTMLChunks).end();
+
+
+
+// function getAndPrintHTMLChunks () {
+
+//   var requestOptions = {
+//     host: 'sytantris.github.io',
+//     path: '/http-examples/step1.html'
+//   };
+
+//   /* Add your code here */
+
+// }
 
 
 
