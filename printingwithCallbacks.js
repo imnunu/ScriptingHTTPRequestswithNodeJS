bc6aa228d915827287f245eff20ecc07176a5bcf
Ellen Chang <imnunu@gmail.com>
2017-03-13 21:18:59 +0000

    first commit

---
+++ printingwithCallbacks.js
@@ -0,0 +1,40 @@
+// function getHTML (options, callback) {
+//   /* Add your code here */
+// }
+
+// function printHTML (html) {
+//   console.log(html);
+// }
+
+// var requestOptions = {
+//   host: 'sytantris.github.io',
+//   path: '/http-examples/step4.html'
+// };
+//===========================================
+
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(requestOptions, function(response) {
+    var data = [];
+    response.on('data', function(chunk){
+      data.push(chunk);
+    }).on('end',function() {
+      var buffer = Buffer.concat(data);
+      console.log(buffer.toString());
+      });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
