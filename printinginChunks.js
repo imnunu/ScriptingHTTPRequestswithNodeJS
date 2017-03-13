3d0168fac6de092905850bd717d2addd683a0a77
Ellen Chang <imnunu@gmail.com>
2017-03-13 20:50:58 +0000

    re-do

--- printinginChunks.js
+++ printinginChunks.js
@@ -1,53 +1,30 @@
-// var https = require('https');
-
-// var requestOptions = {
-//     host: 'sytantris.github.io',
-//     path: '/http-examples/step1.html'
-//   };
-
-// https.get(requestOptions, function(response) {
-//   var bodyChunks = [];
-//   response.on('data', function(chunk){
-//     bodyChunks.push(chunk);
-//   }).on('end', function() {
-//     var body = Buffer.concat(bodyChunks);
-//     console.log('BODY: ' + body);
-//   })
-// });
-
-
-var https = require('https');
-
-var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
-  };
-
-var getAndPrintHTMLChunks = function(response) {
-// https.get(requestOptions, function(response) {
-
-  response.on('data', function(chunk){
-    // console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-    console.log(chunk.toString() + '\n');
-  })
-};
-
-https.request(requestOptions, getAndPrintHTMLChunks).end();
-
-
-
 // function getAndPrintHTMLChunks () {
-
 //   var requestOptions = {
 //     host: 'sytantris.github.io',
 //     path: '/http-examples/step1.html'
 //   };
-
 //   /* Add your code here */
-
 // }
+//=========================================
 
+var https = require('https');
 
+function getAndPrintHTMLChunks () {
 
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+  /* Add your code here */
 
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf-8');
+    response.on('data', function(chunk){
 
+    console.log(chunk.toString() + '\n');
+    })
+
+  });
+}
+
+getAndPrintHTMLChunks();
--- printingwithBufferedStrings.js
+++ printingwithBufferedStrings.js
@@ -1,18 +1,29 @@
+// function getAndPrintHTML () {
+//   var requestOptions = {
+//     host: 'sytantris.github.io',
+//     path: '/http-examples/step2.html'
+//   };
+//   /* Add your code here */
+// }
+//=========================================
 var https = require('https');
 
-var requestOptions = {
+function getAndPrintHTML () {
+
+  var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
   };
 
-https.get(requestOptions, function(response) {
-  var bodyChunks = [];
-  response.on('data', function(chunk){
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-    var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
-  })
-});
-
-
+  /* Add your code here */
+  https.get(requestOptions, function(response) {
+    var data = [];
+    response.on('data', function(chunk){
+      data.push(chunk);
+    }).on('end',function() {
+      var buffer = Buffer.concat(data);
+      console.log(buffer.toString());
+      });
+  });
+}
+getAndPrintHTML();
